8357da9c630efe48eceb1f677f62d54338f98154
hoanghieu.dang@gmail.com <hoanghieu.dang@gmail.com>
2025-01-13 10:32:34 +0100

    adding Limited Product and Non Stocked Product Class and fix some methods of those classes

--- src/products.py
+++ src/products.py
@@ -2,6 +2,7 @@ class Product:
     """
     class Product to handle all information of a product
     """
+
     def __init__(self, name, price, quantity):
         """
         Initialize a Product object
@@ -77,17 +78,88 @@ class Product:
         """
         reduce the total quantity of the product by the quantity passed as argument
         """
-        if is_int_type_check(quantity):
-            if 0 < quantity <= self._quantity:
-                self._quantity -= quantity
-                if self._quantity == 0:
-                    self.deactivate()
-                return quantity * self._price
-            elif quantity < 0:
-                raise Exception("please give a quantity larger than 0!")
-            else:
-                raise Exception(f"not enough {self._name} in the warehouse")
+        if self._active:
+            if is_int_type_check(quantity):
+                if 0 < quantity <= self._quantity:
+                    self._quantity -= quantity
+                    if self._quantity == 0:
+                        self.deactivate()
+                    return quantity * self._price
+                elif quantity < 0:
+                    raise Exception("please give a quantity larger than 0!")
+                else:
+                    raise Exception(f"not enough {self._name} in the warehouse")
+        else:
+            raise Exception("Product is not active")
 
+
+# __________________________________________________________________________________________
+class NonStockedProduct(Product):
+    """
+    Digital Product inherited from class Product
+    """
+
+    def __init__(self, name, price):
+        quantity = 1
+        super().__init__(name, price, quantity)
+        # since it is a NonStockedProduct, the _quantity will always be set to 0
+        self._quantity = 0
+
+    def get_quantity(self):
+        """
+        returns the quantity of the product
+        """
+        return 0
+
+    def set_quantity(self, quantity):
+        """
+        set the quantity of the product
+        """
+        raise Exception("Can't set quantity for this non stocked product")
+
+    def show(self):
+        """
+        return the information of the product
+        """
+        return f"{self._name}, Price: {self._price}, non stocked product"
+
+    def buy(self):
+        """
+        since it is a non stocked product, the product can not be ordered more than once
+        """
+        if self._active:
+            return self._price
+
+
+# __________________________________________________________________________________________
+class LimitedProduct(Product):
+    def __init__(self, name, price, quantity, maximum):
+        super().__init__(name, price, quantity)
+        self._maximum = maximum
+
+    def buy(self, quantity):
+        """
+        reduce the total quantity of the product by the quantity passed as argument
+        """
+        if self._active:
+            if is_int_type_check(quantity):
+                if 0 < quantity <= self._quantity:
+                    if quantity <= self._maximum:
+                        self._quantity -= quantity
+                        if self._quantity == 0:
+                            self.deactivate()
+                        return quantity * self._price
+                    else:
+                        raise Exception(f"please order a quantity less than {self._maximum}")
+                elif quantity < 0:
+                    raise Exception("please give a quantity larger than 0!")
+                else:
+                    raise Exception(f"not enough {self._name} in the warehouse")
+        else:
+            raise Exception("Product is not active")
+
+
+# __________________________________________________________________________________________
 def is_int_type_check(num):
     """
     check whether a variable has the type of integer
@@ -96,6 +168,7 @@ def is_int_type_check(num):
         return True
     else:
         raise Exception("please only enter an integer!")
+
 
 def is_int_or_float_type_check(num):
     """
@@ -106,6 +179,7 @@ def is_int_or_float_type_check(num):
     else:
         raise Exception("please only enter an integer or a float!")
 
+
 def is_str_type_check(input_str):
     """
     check whether a variable has the type of string
@@ -114,5 +188,3 @@ def is_str_type_check(input_str):
         return True
     else:
         raise Exception("please only enter a string!")
-
-
